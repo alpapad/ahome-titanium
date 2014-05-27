18df5c7866dbef83fa99ef6699e28687e29d44fb
eemi2010 <jazzmatadazz@gmail.com>
2014-05-27 18:37:54 +0200

    started examples package

---
+++ ahome-titanium/examples/com/ait/toolkit/titanium/mobile/examples/helloworld/HelloTi.java
@@ -0,0 +1,12 @@
+package com.ait.toolkit.titanium.mobile.examples.helloworld;
+
+import com.ait.toolkit.titanium.mobile.client.core.TiEntryPoint;
+
+public class HelloTi extends TiEntryPoint {
+
+	@Override
+	public void onStart() {
+
+	}
+
+}
